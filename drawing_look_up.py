c1a53ef3e8473293d094429a6a11d610559b97a3
Protonwaven <47987579+Protonwaven@users.noreply.github.com>
2019-08-10 05:07:29 -0400

    Update to load with clipboard data
    Generally cleaned up code and added the option to start with the drawing in the clip board and begin the search with that instead of entering it manually.

--- drawing_look_up.py
+++ drawing_look_up.py
@@ -2,7 +2,7 @@
     .mpr file if they choose.
 
     Inputs:
-        drawingNumber -- This is the drawing number excluding any revisions.
+        drawingNumber -- This is the drawing number excluding any revision information.
 
     Outputs:
         A printed list of which machines the drawing is on, if none then it will offer to create file using Gen_rec.py.
@@ -10,21 +10,38 @@
 """
 import os
 import time
+import machineFilePaths
+import pyperclip
 
-# list of machine pathways, if it exists, and machine names
-vantechPath, vantech, van = '\\\\nts141\\KOShopFloor\\Common\\Vantech\\Parts', False, 'Vantech'
-jasperWeekePath, jasperWeeke, jas = '\\\\nts141\\KOShopFloor\\15th St\\JasperWeeke', False, 'JasperWeeke'
-fvWeekePath, fvWeeke, fv = '\\\\nts141\\KOShopFloor\\15th St\\FVWeeke\\mp4', False, 'FVWeeke'
-bazPath, baz, BAZ = '\\\\nts141\\KOShopFloor\\15th St\\Homag_BAZ\\BAZ\\mp4', False, 'BAZ'
-abdPath, abd, ABD = '\\\\nts141\\KOShopFloor\\Common\\Homaghbore', False, 'Homaghbore'
+# list of machine pathways, setting 'if it exists' to false by default, and machine names
+vantechPath, vantech, van = machineFilePaths.vantech_parts(), False, 'Vantech'
+jasperWeekePath, jasperWeeke, jas = machineFilePaths.jasper_parts(), False, 'JasperWeeke'
+fvWeekePath, fvWeeke, fv = machineFilePaths.fordsvile_parts(), False, 'FVWeeke'
+bazPath, baz, BAZ = machineFilePaths.baz_parts(), False, 'BAZ'
+abdPath, abd, ABD = machineFilePaths.abd_parts(), False, 'Homaghbore'
 
 allPaths = [vantechPath, jasperWeekePath, fvWeekePath, bazPath, abdPath]
 foundOnMachine = [vantech, jasperWeeke, fvWeeke, baz, abd]
 machineNames = [van, jas, fv, BAZ, ABD]
 fileList = {}
 
-drawingNumber = input(str('Please enter the drawing number you want to find.\n'))
+# input for drawing look up
+# first option is to use the clipboard. Try/Except to validate input is an integer, and then confirm length of input.
+# if clipboard isn't validated or user doesn't confirm look up by entering text, then ask for the drawing number to
+# look up.
+try:
+    if int(pyperclip.paste()) >= 6 & int(pyperclip.paste()) <= 8:
+        use_clipboard = input('Type anything to look up ' + str(pyperclip.paste()) + '. ')
+        if use_clipboard:
+            drawingNumber = pyperclip.paste()
+        else:
+            drawingNumber = input(str('Please enter the drawing number you want to find.\n'))
+    else:
+        drawingNumber = input(str('Please enter the drawing number you want to find.\n'))
+except ValueError:
+    drawingNumber = input(str('Please enter the drawing number you want to find.\n'))
 
+# Begin looking for drawing number in machine files
 while drawingNumber:
     fileListKey = 1
     for path in allPaths:
@@ -33,15 +50,13 @@ while drawingNumber:
                 if filename.endswith('.mpr') and filename.startswith(drawingNumber):
                     for i in range(len(machineNames)):
                         if machineNames[i] in path:
-                            # Get Last Modified Data
                             fullpath = os.path.join(rootDir, filename)
-
-                            # Get file
                             foundOnMachine[i] = True
                             print(str(fileListKey) + ': ' + filename + ' found on the ' + machineNames[i]
                                   + ' : ' + time.ctime(os.path.getctime(fullpath)))
                             fileList.update({fileListKey: os.path.join(rootDir, filename)})
                             fileListKey += 1
+
     # If drawing doesn't exist, offer to make it.
     while not any(foundOnMachine):
         create_file = input('Would you like to create ' + drawingNumber + '?\nOr hit enter to escape.\n')
@@ -49,7 +64,7 @@ while drawingNumber:
             os.startfile("C:\\Users\\scasey\\gen_rec2.bat")
         break
 
-    # Ask which machines, if any, the user wants to open the file up from.
+    # Ask which machines, if any, the user wants to open the file up from and prompt for next file to look up
     while any(foundOnMachine):
         print('Type the line number to open the file or just hit enter to look up another file.')
         fileToOpen = input(str(''))
