a21cf01d9c0dc8b916035eca77c31c36dd0e5855
Protonwaven <47987579+Protonwaven@users.noreply.github.com>
2019-08-10 06:06:30 -0400

    Update code
    Added loop to prompt for correct input of path to scan and cleaned up the code.

--- scanAndCountString.py
+++ scanAndCountString.py
@@ -1,40 +1,46 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Jun  7 09:28:30 2019
+A program to search all .mpr file within a specific directory for a certin string, commonly a component name, and
+count how many times it appears. Then print the results to a new file and open file for review.
 
-@author: scasey
+Inputs:
+    scanFolder -- this is the folder you wish to scan. It must be entered as an absolute path.
+        example: c:\\python\\quotes
+    findString -- This is the string you wish to find.
+        example: Tis but a scratch
+
+Outputs:
+    output -- A file with the results of the scanning and counting.
 """
+import os
+import re
 
-# search each file in a folder for a string, then print the name 
-# of each file with that string to another file.
+# Get the path to scan.Then run a quick regex check to see if the input matches the basic windows path
+# format. If the matching fails, reprompt for correct input
+scanFolder = input('Select absolute path to the folder to scan:\n')
+validatePathRegex = re.compile(r'[a-z,A-Z]:\\?(\\?\w*\\?)*')
+mo = validatePathRegex.search(scanFolder)
+while not mo:
+    print('Path is not valid. Please re-enter path.')
+    scanFolder = input('Please renter the absolute path to the folder to scan:\n')
+    mo = validatePathRegex.search(scanFolder)
 
-import os, re
+# Enter the string you wish to find.
+findString = input('Enter the text to find:\n' )
 
-scanFolder = input('Select absolute path to folder to scan:\n')                 #get file dir that you want to scan
-
-validatePathRegex = re.compile(r'[a-z,A-Z]:\\?(\\?\w*\\?)*')                       #this checks for a valid path format
-mo = validatePathRegex.search(scanFolder)                                          #exp. c:\spam\bacon\
-if mo == None:
-    print('Path is not valid. Please re-enter path.\n')
-    import sys
-    sys.exit()
-
-os.chdir(scanFolder)                                                            #chdir to scanFolder 
-findString = input('Enter the text to find:\n' )                                #get text string to search for
-
+os.chdir(scanFolder)
 count = 0
 outputList = []
-with open('found_%s_or_not.txt' % findString, 'w') as output:                   #open output with context manager
-    for root, subdir, files in os.walk(scanFolder):                             #begin walking down file tree 
-        for file in files:                                  
-                if os.path.join(file).endswith('.mpr'):                             #confirm we're only checking MPR files                
-                    fullpath = os.path.join(root, file)                             #create the fullpath to each file                
-                    with open(fullpath, 'r') as f:                                  #open file with context manager
-                        for lines in file:
-                            line = f.read()                                         #read line in file
-                            if findString in line:                                  #if findString found in file, write fullpath to output                             
-                                outputList.append(os.path.join(root, file) + ' contains ' + findString + '\n') 
-                                count += 1
+with open('found_%s_or_not.txt' % findString, 'w') as output:
+    for root, subdir, files in os.walk(scanFolder):
+        for file in files:
+            if os.path.join(file).endswith('.mpr'):
+                fullpath = os.path.join(root, file)
+                with open(fullpath, 'r') as f:
+                    for lines in file:
+                        line = f.read()
+                        if findString in line:
+                            outputList.append(os.path.join(root, file) + ' contains ' + findString + '\n')
+                            count += 1
     outputList = ['The total count is ' + str(count) + '\n\n'] + outputList
     output.write(''.join(outputList))
-print('Done')
+    os.startfile(output)
