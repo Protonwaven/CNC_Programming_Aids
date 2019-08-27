1e4869a6f22603e9a889835a4cd576c5cd3bf5a5
Protonwaven <47987579+Protonwaven@users.noreply.github.com>
2019-08-27 05:14:10 -0400

    Added to docstring and a reminder to notify other programmers of the changes.

--- updateML4.py
+++ updateML4.py
@@ -1,4 +1,11 @@
-"""A program to update the ML4 files from the ML4 folder on common and sending it to all machines.
+"""
+    A program to update the ML4 files from the ML4 folder on common and sending it to all machines.
+
+    Input:
+        None.
+
+    Output:
+        A print out in the terminal of all files that have been updated.
 
 """
 
@@ -23,7 +30,8 @@ for folderName, subfolders, filenames in os.walk(ML4):
             for path in Machines:
                 shutil.copy(fullpath, path)
                 print('Copying from {} to {}'.format(fullpath, path))
-print('\nAll files have been updated.\n')
+print('\nAll files have been updated.\n\nNow please send an email to all other programmers to remind them to update '
+      'their folders has well.\n')
 
-# Added sleep(6) to allow the user to read the terminal. 
-time.sleep(6)
+# Added sleep(12) to allow the user to read the terminal.
+time.sleep(12)
