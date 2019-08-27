2070a062dbda5d4de1e069b7f9f5bb407ed18a66
Protonwaven <47987579+Protonwaven@users.noreply.github.com>
2019-08-27 08:46:14 -0400

    added built in check to compare ML4 files and only update it needed

--- updateML4.py
+++ updateML4.py
@@ -1,5 +1,6 @@
 """
-    A program to update the ML4 files from the ML4 folder on common and sending it to all machines.
+    A program to update the ML4 files from the ML4 folder on common and sending it to all machines if the Common version
+    has been modified more recently than the version on the machine. This should reduce unnecessary updating.
 
     Input:
         None.
@@ -22,16 +23,26 @@ Fordsville_ML4 = machineFilePaths.fordsvile_ml4()
 BAZ_ML4 = machineFilePaths.baz_ml4()
 
 Machines = [Vantech_ML4, Jasper_ML4, Fordsville_ML4, BAZ_ML4]
+updated_files_counter = 0
 
 for folderName, subfolders, filenames in os.walk(ML4):
     for filename in filenames:
         if filename.endswith('.mpr'):
             fullpath = os.path.join(folderName, filename)
+            fullpath_mtime = os.path.getmtime(fullpath)
             for path in Machines:
-                shutil.copy(fullpath, path)
-                print('Copying from {} to {}'.format(fullpath, path))
-print('\nAll files have been updated.\n\nNow please send an email to all other programmers to remind them to update '
-      'their folders has well.\n')
+                ml4_file = os.path.join(path, filename)
+                ml4_file_mtime = os.path.getmtime(ml4_file)
+                # Comparing mtimes to determine which file is newer and if it needs replaced.
+                if ml4_file_mtime < fullpath_mtime:
+                    shutil.copy(fullpath, path)
+                    print('Copying from {} to {}'.format(fullpath, path))
+                    updated_files_counter += 1
+if updated_files_counter >= 1:
+    print('\n{} files have been updated.\n\nNow please send an email to all other programmers to remind them to update '
+          'their folders has well.\n'.format(str(updated_files_counter)))
+elif updated_files_counter < 1:
+    print('\nNo files were updated.\n\n')
 
 # Added sleep(12) to allow the user to read the terminal.
 time.sleep(12)
