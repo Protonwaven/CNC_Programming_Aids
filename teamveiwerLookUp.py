739e0925193bb685422d0e0ad53c3aa297781eb4
Protonwaven <47987579+Protonwaven@users.noreply.github.com>
2019-09-18 08:43:32 -0400

    added new file teamveiwerLookUp.py to automate opening and pasting drawing numbers for look up.

---
+++ teamveiwerLookUp.py
@@ -0,0 +1,29 @@
+"""A quick script to pull up TeamCenter and search for the drawing using GUI
+automation.
+
+    Input -- Will load drawing number from Windows clipboard, or terminal.
+"""
+
+import pyautogui
+import pyperclip
+
+try:
+    if int(pyperclip.paste()) >= 6 & int(pyperclip.paste()) <= 8:
+        use_clipboard = input(f'Type 1 to look up {str(pyperclip.paste())}'
+                              f'\nElse, type in the drawing number you want '
+                              f'to find.\n')
+        if use_clipboard == '1':
+            drawingNumber = pyperclip.paste().strip()
+        else:
+            drawingNumber = use_clipboard
+    else:
+        drawingNumber = input(str('Please enter the drawing number you want to find.\n'))
+except ValueError:
+    drawingNumber = input(str('Please enter the drawing number you want to find.\n'))
+
+iePosition = (91, 1059)
+
+pyautogui.click(iePosition)
+pyautogui.sleep(2)
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.hotkey('enter')
